1af65e2373f4d8abe9fe7822769eb3b669d7b3a1
rezamax <reza.max555@gmail.com>
2020-12-01 23:11:07 +0330

    Initial Commit

---
+++ GorillaWebSocket.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home Page")
+}
+
+func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+	upgrader.CheckOrigin = func(r *http.Request) bool {
+		return true
+	}
+
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	log.Println("Client Connected")
+
+	err = ws.WriteMessage(1, []byte("Hi Client!"))
+	if err != nil {
+		log.Println(err)
+	}
+
+	reader(ws)
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		_, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		fmt.Println(string(p))
+
+		var sendMsg string
+		fmt.Print("Enter message: ")
+		fmt.Scanf("%s", &sendMsg)
+
+		if sendMsg != "" {
+			err = conn.WriteMessage(1, []byte(sendMsg))
+			if err != nil {
+				log.Println(err)
+			}
+		}
+		// if string(p) == "How Are you" {
+		// 	err = conn.WriteMessage(1, []byte("I'm good!"))
+		// 	if err != nil {
+		// 		log.Println(err)
+		// 	}
+		// }
+	}
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/ws", wsEndpoint)
+}
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func main() {
+	fmt.Println("Hello World!")
+	setupRoutes()
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
